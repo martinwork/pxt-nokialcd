dc6a006488cd5b95d28e09b19702c1c6b7dce84b
Michael Killpatrick <63163207+MTKilpatrick@users.noreply.github.com>
2020-05-10 12:13:35 +0100

    Update shims.d.ts

--- shims.d.ts
+++ shims.d.ts
@@ -1,2 +1,9 @@
 declare namespace nokialcd {
+      /**
+     * Blah
+     */
+    //% blockId="nokia_writeBuf"
+    //% block="blah %data"
+    //% shim=nokialcd::writeBuf
+    function writeBuf(data: Buffer): void;
 }
